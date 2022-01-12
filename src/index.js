721f9445736245b7ecc5a2a6f56c977c8ed76c89
Pavel Ivanov <pablospaniard@users.noreply.github.com>
2022-01-12 15:56:32 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import Layout from './containers/Layout/Layout'
+import reducer from './store/reducer'
+import PhoneListContainer from './containers/PhoneListContainer/PhoneListContainer'
+import PhoneDetailComponent from './components/PhoneDetailComponent/PhoneDetailComponent'
+
+const store = createStore(reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" exact element={<Layout />} />
+                <Route path="/phones" exact element={<PhoneListContainer />} />
+                <Route
+                    path={'/phones/:id'}
+                    element={<PhoneDetailComponent />}
+                />
+                <Route render={() => <h1>Not Found</h1>} />
+            </Routes>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+)
