b18b64308e85e4b5123bf40fdd22732f851a5df5
e-schultz <evan@schultz.codes>
2022-01-19 16:40:51 -0500

    .... lost

--- src/components/PhoneDetailComponent/PhoneDetailComponent.test.js
+++ src/components/PhoneDetailComponent/PhoneDetailComponent.test.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { render as rtlRender, screen, prettyDOM } from "@testing-library/react";
+import { createMemoryHistory } from "history";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { Router, Route } from "react-router-dom";
 // Import our reducer
 import reducer from "../../store/reducer"; // to access rest of reducers
 import PhoneDetailComponent from "./PhoneDetailComponent";
@@ -9,6 +11,8 @@ import PhoneDetailComponent from "./PhoneDetailComponent";
 const render = (
   ui,
   {
+    route = "/",
+    history = createMemoryHistory({ initialEntries: [route] }),
     initialState,
     store = createStore(reducer, initialState),
     ...renderOptions
@@ -17,11 +21,23 @@ const render = (
   const Wrapper = ({ children }) => {
     return <Provider store={store}>{children}</Provider>;
   };
-  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+  return rtlRender(
+    <Router history={history}>
+      <Route path={route}>{ui}</Route>
+    </Router>,
+    { wrapper: Wrapper, ...renderOptions, history }
+  );
 };
 describe("magic", () => {
   it("should work", () => {
-    const comp = render(<PhoneDetailComponent />);
+    const comp = render(
+      <Route path="/phones/:id">
+        <PhoneDetailComponent />
+      </Route>,
+      {
+        route: "/phones/1",
+      }
+    );
     const phone1 = comp.getByTestId("phone-idx-1");
     screen.getByTestId("phone-idx-1");
   });
