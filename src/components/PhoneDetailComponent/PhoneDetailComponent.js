3a8cc1d29fd4876414b6c251eb6d89053e0fd99b
e-schultz <evan@schultz.codes>
2022-01-19 16:28:09 -0500

    testing a single component
    something I didn't realize - useParams from the react router dom
    
    I didn't realize that this was getting all of the components from the store,
    
    then filteringt down by the paramater

--- src/components/PhoneDetailComponent/PhoneDetailComponent.js
+++ src/components/PhoneDetailComponent/PhoneDetailComponent.js
@@ -1,67 +1,62 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Link, useParams } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link, useParams } from "react-router-dom";
 
 const renderPhone = (phone, index) => (
-    <div>
-        <div className="jumbotron justify-content-center d-flex" key={index}>
-            <div className="col-md-6">
-                <Link
-                    to="/phones"
-                    className="btn btn-succes d-flex justify-content-center"
-                >
-                    Back to catalog
-                </Link>
-                <div className="img-thumbnail row">
-                    <div className="col-md-4">
-                        <img
-                            src={phone.image}
-                            alt={phone.name}
-                            className="img-thumbnail"
-                        />
-                    </div>
-                    <div className="col-md-8">
-                        <ul>
-                            <li>CPU:{phone.cpu}</li>
-                            <li>CAMERA:{phone.camera}</li>
-                            <li>SIZE:{phone.size}</li>
-                            <li>WEIGHT:{phone.weight}</li>
-                            <li>DISPLAY:{phone.display}</li>
-                            <li>BATTERY:{phone.battery}</li>
-                            <li>MEMORY:{phone.memory}</li>
-                        </ul>
-                    </div>
-                    <div className="caption col-md-12">
-                        <h4 className="justify-content-end d-flex">
-                            $ {phone.price}
-                        </h4>
-                        <h4>{phone.name}</h4>
-                    </div>
-                    <div>
-                        <p className="text-center">{phone.description}</p>
-                    </div>
-                </div>
-            </div>
+  <div>
+    <div className="jumbotron justify-content-center d-flex" key={index}>
+      <div className="col-md-6">
+        <Link
+          to="/phones"
+          className="btn btn-succes d-flex justify-content-center"
+        >
+          Back to catalog
+        </Link>
+        <div className="img-thumbnail row" data-testid={`phone-idx-${index}`}>
+          <div className="col-md-4">
+            <img src={phone.image} alt={phone.name} className="img-thumbnail" />
+          </div>
+          <div className="col-md-8">
+            <ul>
+              <li>CPU:{phone.cpu}</li>
+              <li>CAMERA:{phone.camera}</li>
+              <li>SIZE:{phone.size}</li>
+              <li>WEIGHT:{phone.weight}</li>
+              <li>DISPLAY:{phone.display}</li>
+              <li>BATTERY:{phone.battery}</li>
+              <li>MEMORY:{phone.memory}</li>
+            </ul>
+          </div>
+          <div className="caption col-md-12">
+            <h4 className="justify-content-end d-flex">$ {phone.price}</h4>
+            <h4>{phone.name}</h4>
+          </div>
+          <div>
+            <p className="text-center">{phone.description}</p>
+          </div>
         </div>
+      </div>
     </div>
-)
+  </div>
+);
 
 const PhoneDetailComponent = ({ phones }) => {
-    const params = useParams()
-    return (
-        <div>
-            <div className="row">
-                {phones.map((phone, index) =>
-                    index === Number(params.id) ? renderPhone(phone, index) : ''
-                )}
-            </div>
-        </div>
-    )
-}
+  const params = useParams();
+  return (
+    <div>
+      <div className="row">
+        {phones.map((phone, index) => {
+          console.log("hi", phone);
+          return index === Number(params.id) ? renderPhone(phone, index) : "";
+        })}
+      </div>
+    </div>
+  );
+};
 
-const mapStateToProps = state => {
-    console.log('StateToProps', state)
-    return { phones: state.phones }
-}
+const mapStateToProps = (state) => {
+  console.log("StateToProps", state);
+  return { phones: state.phones };
+};
 
-export default connect(mapStateToProps)(PhoneDetailComponent)
+export default connect(mapStateToProps)(PhoneDetailComponent);
--- src/components/PhoneDetailComponent/PhoneDetailComponent.test.js
+++ src/components/PhoneDetailComponent/PhoneDetailComponent.test.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import {render as rtlRender, screen} from '@testing-library/react';
-import PhoneDetailComponent from './PhoneDetailComponent';
-const render = (ui) => {
-    return rtlRender(ui);
-}
-describe('magic',()=>{
-    it('should work',()=>{
-        render(<PhoneDetailComponent/>)
-        expect(1).toEqual(2);
-    });
-})
+import React from "react";
+import { render as rtlRender, screen, prettyDOM } from "@testing-library/react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+// Import our reducer
+import reducer from "../../store/reducer"; // to access rest of reducers
+import PhoneDetailComponent from "./PhoneDetailComponent";
+
+const render = (
+  ui,
+  {
+    initialState,
+    store = createStore(reducer, initialState),
+    ...renderOptions
+  } = {}
+) => {
+  const Wrapper = ({ children }) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+};
+describe("magic", () => {
+  it("should work", () => {
+    const comp = render(<PhoneDetailComponent />);
+    const phone1 = comp.getByTestId("phone-idx-1");
+    screen.getByTestId("phone-idx-1");
+  });
+});
