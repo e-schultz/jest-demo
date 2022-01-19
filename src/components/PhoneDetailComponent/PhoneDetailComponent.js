4bafc31d37dd9bb9de2eb4fcdd44e3caa1809b95
e-schultz <evan@schultz.codes>
2022-01-19 17:45:12 -0500

    add the div back to clean up the diff a bit

--- src/components/PhoneDetailComponent/PhoneDetailComponent.js
+++ src/components/PhoneDetailComponent/PhoneDetailComponent.js
@@ -3,35 +3,37 @@ import { connect } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 
 const renderPhone = (phone, index) => (
-  <div className="jumbotron justify-content-center d-flex" key={index}>
-    <div className="col-md-6">
-      <Link
-        to="/phones"
-        className="btn btn-succes d-flex justify-content-center"
-      >
-        Back to catalog
-      </Link>
-      <div className="img-thumbnail row" data-testid={`phone-id-${phone.id}`}>
-        <div className="col-md-4">
-          <img src={phone.image} alt={phone.name} className="img-thumbnail" />
-        </div>
-        <div className="col-md-8">
-          <ul>
-            <li>CPU:{phone.cpu}</li>
-            <li>CAMERA:{phone.camera}</li>
-            <li>SIZE:{phone.size}</li>
-            <li>WEIGHT:{phone.weight}</li>
-            <li>DISPLAY:{phone.display}</li>
-            <li>BATTERY:{phone.battery}</li>
-            <li>MEMORY:{phone.memory}</li>
-          </ul>
-        </div>
-        <div className="caption col-md-12">
-          <h4 className="justify-content-end d-flex">$ {phone.price}</h4>
-          <h4>{phone.name}</h4>
-        </div>
-        <div>
-          <p className="text-center">{phone.description}</p>
+  <div key={index}>
+    <div className="jumbotron justify-content-center d-flex">
+      <div className="col-md-6">
+        <Link
+          to="/phones"
+          className="btn btn-succes d-flex justify-content-center"
+        >
+          Back to catalog
+        </Link>
+        <div className="img-thumbnail row" data-testid={`phone-id-${phone.id}`}>
+          <div className="col-md-4">
+            <img src={phone.image} alt={phone.name} className="img-thumbnail" />
+          </div>
+          <div className="col-md-8">
+            <ul>
+              <li>CPU:{phone.cpu}</li>
+              <li>CAMERA:{phone.camera}</li>
+              <li>SIZE:{phone.size}</li>
+              <li>WEIGHT:{phone.weight}</li>
+              <li>DISPLAY:{phone.display}</li>
+              <li>BATTERY:{phone.battery}</li>
+              <li>MEMORY:{phone.memory}</li>
+            </ul>
+          </div>
+          <div className="caption col-md-12">
+            <h4 className="justify-content-end d-flex">$ {phone.price}</h4>
+            <h4>{phone.name}</h4>
+          </div>
+          <div>
+            <p className="text-center">{phone.description}</p>
+          </div>
         </div>
       </div>
     </div>
